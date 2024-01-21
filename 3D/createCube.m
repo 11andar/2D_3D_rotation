f44bba3f244377355f3c885fff37a999872ac22a
ad <adarowski@student.agh.edu.pl>
2024-01-21 21:11:03 +0100

    Moved to 2D_3D_rotation

--- 3D/createCube.m
+++
@@ -1,7 +0,0 @@
-function [vertices, faces] = createCube()
-    % vertices - vector containing (x, y, z) coordinates of each vertex
-    % faces - vector containing indices of vertices to connect to create
-            % faces
-    vertices = [-1 -1 -1; 1 -1 -1; 1 1 -1; -1 1 -1; -1 -1 1; 1 -1 1; 1 1 1; -1 1 1];
-    faces = [1 2 6 5; 2 3 7 6; 3 4 8 7; 4 1 5 8; 1 2 3 4; 5 6 7 8];
-end
